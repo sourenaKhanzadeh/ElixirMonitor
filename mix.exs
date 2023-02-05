227f1cdd65f821f2599e1370b73f9a6f857c1961
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2023-02-04 21:08:57 -0500

    project deps initiated

---
+++ mix.exs
@@ -0,0 +1,18 @@
+defmodule ElixirMonitor.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :elixir_monitor,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
+
